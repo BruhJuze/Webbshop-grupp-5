f92fe7b5e09405f540c96049fc059393cc99f5d5
Christopher <hej@christopherwilliams.se>
2022-12-09 14:46:48 +0100

    Visual styling for navigation section.

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,18 @@
 const hamburger = document.querySelector('.hamburger');
+const cartButton = document.querySelector('.cart-button');
 const mobileMenu = document.querySelector('.mobile-menu');
+const cart = document.querySelector('.cart');
+const closeButton = document.querySelector('.close-button');
 
 hamburger?.addEventListener('click', function(){
     hamburger?.classList.toggle('is-active');
     mobileMenu?.classList.toggle('is-active');
+});
+
+cartButton?.addEventListener('click', function(){
+    cart?.classList.add('is-active');
+});
+
+closeButton?.addEventListener('click', function(){
+    cart?.classList.remove('is-active');
 });
