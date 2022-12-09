cbeb5891c89cee89b88fb9d099f07fe41719807b
Christopher <hej@christopherwilliams.se>
2022-12-09 13:19:54 +0100

    Updated functionality for mobile menu

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 
-hamburger.addEventListener('click', function(){
-    this.classList.toggle('is-active');
-    mobileMenu.classList.toggle('is-active');
+hamburger?.addEventListener('click', function(){
+    hamburger?.classList.toggle('is-active');
+    mobileMenu?.classList.toggle('is-active');
 });
