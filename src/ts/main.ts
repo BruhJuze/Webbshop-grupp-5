986007aa95344fd3cb780e225cd78db5bb5ee975
Christopher <hej@christopherwilliams.se>
2022-12-23 12:57:52 +0100

    first example of dropdown version to be reviewed

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -5,6 +5,8 @@ const cart = document.querySelector('.cart');
 const closeButton = document.querySelector('.close-button');
 const navbar = document.querySelector('nav');
 const logo = document.querySelector('.logo-png');
+const dropdownMenu = document.querySelector('.dropdown');
+const dropdownMenu2 = document.querySelector('.dropdown2');
 
 hamburger?.addEventListener('click', () => {
     hamburger?.classList.toggle('is-active');
@@ -26,10 +28,13 @@ window.addEventListener('scroll', () => {
         navbar?.classList.remove('is-scrolled');
         logo?.classList.remove('is-scrolled');
         mobileMenu?.classList.remove('is-scrolled');
+        dropdownMenu?.classList.remove('is-scrolled');
+        dropdownMenu2?.classList.remove('is-scrolled');
     } else {
         navbar?.classList.add('is-scrolled');
         logo?.classList.add('is-scrolled');
-        mobileMenu?.classList.add('is-scrolled');
+        dropdownMenu?.classList.add('is-scrolled');
+        dropdownMenu2?.classList.add('is-scrolled');
     }
 
     console.log(hScroll);
