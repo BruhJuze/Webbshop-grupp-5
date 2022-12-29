ea36c4964a480a87dfff43116e291d8fc33e6ec1
bruhJuze <mikailerdogan19@gmail.com>
2022-12-29 17:06:52 +0100

    done

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -41,6 +41,8 @@ window.addEventListener('scroll', () => {
 
 //Mikails kodning______________________________________________________________________________________________________________________________________
 
+let purchaseBtn:HTMLButtonElement = document.getElementById("purchaseBtn") as HTMLButtonElement;
+
 function checkoutLoad() {
     location.href = "/checkout.html";
     console.log("load");
@@ -48,19 +50,88 @@ function checkoutLoad() {
 
 checkoutBtn.addEventListener("click", checkoutLoad);
 
-let newProduct: HTMLDivElement = document.createElement("div") as HTMLDivElement;
+/*function completeLoad() {
+    location.href = "/order-done.html";
+    console.log("load");
+    }
+ 
+purchaseBtn.addEventListener("click", completeLoad);
+*/
 let totalPrice: HTMLDivElement = document.getElementById("totalPrice") as HTMLDivElement;
 let bTag: HTMLBaseElement = document.createElement("b") as HTMLBaseElement;
-newProduct.className = "mainCheckout__newProduct"
 
 
 
-prodContainer.appendChild(newProduct);
-prodContainer.appendChild(newProduct.cloneNode(true));
+
+
+
 //prodContainer.appendChild(newProduct.cloneNode(true));
 //prodContainer.appendChild(newProduct.cloneNode(true));
 //prodContainer.appendChild(newProduct.cloneNode(true));
 //prodContainer.appendChild(newProduct.cloneNode(true));
+
+for(let i=0; i < 5; i++){
+    let newProduct: HTMLDivElement = document.createElement("div") as HTMLDivElement;
+    let title:HTMLElement = document.createElement("section") as HTMLElement;
+    let image:HTMLImageElement = document.createElement("img") as HTMLImageElement;
+    let rightContainer:HTMLDivElement = document.createElement("div") as HTMLDivElement;
+    let leftContainer:HTMLDivElement = document.createElement("div") as HTMLDivElement;
+    let leftChildContainer:HTMLDivElement = document.createElement("div") as HTMLDivElement;
+    let pricePerThing:HTMLElement = document.createElement("section") as HTMLElement;
+    let amountContainer:HTMLDivElement = document.createElement("div") as HTMLDivElement;
+    let sum:HTMLElement = document.createElement("section") as HTMLElement;
+    let removeBtn:HTMLElement = document.createElement("i") as HTMLElement;
+    let amountAdd: HTMLDivElement = document.createElement("div") as HTMLDivElement;
+    let amountSub: HTMLDivElement = document.createElement("div") as HTMLDivElement;
+    let amountNumber: HTMLDivElement = document.createElement("div") as HTMLDivElement;
+    let plusIcon:HTMLElement = document.createElement("i") as HTMLElement;
+    let minusIcon:HTMLElement = document.createElement("i") as HTMLElement;
+    
+
+    newProduct.className = "mainCheckout__newProduct";
+    title.className = "mainCheckout__newProduct__leftContainer__leftChildContainer__title";
+    image.className = "mainCheckout__newProduct__leftContainer__image";
+    rightContainer.className = "mainCheckout__newProduct__rightContainer"
+    leftContainer.className = "mainCheckout__newProduct__leftContainer"
+    sum.className = "mainCheckout__newProduct__rightContainer__sum";
+    removeBtn.className = "mainCheckout__newProduct__rightContainer__remove";
+    removeBtn.className = "bi bi-x-lg";
+    amountContainer.className = "mainCheckout__newProduct__leftContainer__leftChildContainer__amountContainer";
+    leftChildContainer.className = "mainCheckout__newProduct__leftContainer__leftChildContainer";
+    pricePerThing.className = "mainCheckout__newProduct__leftContainer__leftChildContainer__pricePerThing";
+    amountAdd.className = "mainCheckout__newProduct__leftContainer__leftChildContainer__amountContainer__sub";
+    amountNumber.className = "mainCheckout__newProduct__leftContainer__leftChildContainer__amountContainer__amountNumber";
+    amountSub.className = "mainCheckout__newProduct__leftContainer__leftChildContainer__amountContainer__add";
+    plusIcon.className = "bi bi-plus";
+    minusIcon.className = "bi bi-dash";
+
+    title.innerHTML = "title";
+    sum.innerHTML = "1500 kr";
+    image.src = "./assets/Startsidan/HALO.jpeg";
+    pricePerThing.innerHTML = "1500 kr/st."
+    amountNumber.innerHTML = "1";
+
+
+    newProduct.appendChild(leftContainer);
+    newProduct.appendChild(rightContainer);
+    leftContainer.appendChild(image);
+    leftContainer.appendChild(leftChildContainer);
+    leftChildContainer.appendChild(title);
+    leftChildContainer.appendChild(pricePerThing);
+    leftChildContainer.appendChild(amountContainer);
+    amountContainer.appendChild(amountSub);
+    amountContainer.appendChild(amountNumber);
+    amountContainer.appendChild(amountAdd);
+    amountAdd.append(plusIcon);
+    amountSub.append(minusIcon);
+    rightContainer.appendChild(removeBtn);
+    rightContainer.appendChild(sum);
+    
+    prodContainer.appendChild(newProduct);
+    
+
+}
+
 let priceTest: number[] = [2000, 10,];
 
 for(let i = 0; i < priceTest.length; i++){
